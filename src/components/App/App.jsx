9ef256e01a48d0e19220f83e33857360ce46a266
SerhiiBasok <sergeybsk19@gmail.com>
2024-03-21 01:22:13 +0200

    upd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import Profile from "../Profile/Profile.jsx";
 import FriendListItem from "../FriendListItem/FriendListItem.jsx";
 import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
 import userData from "../userData.json";
-import friendsData from "../friendListItem.json";
+import friendsData from "../friendData.json";
 import transactionHistory from "../transactionData.json";
 
 const App = () => {
